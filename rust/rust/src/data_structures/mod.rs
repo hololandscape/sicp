efd3982d9fbc6add6d46f6a485e1d8224b2419c7
aisuko <urakiny@gmail.com>
2021-11-14 18:57:28 +0800

    Add single linked list
    Signed-off-by: aisuko <urakiny@gmail.com>

--- rust/rust/src/data_structures/mod.rs
+++ rust/rust/src/data_structures/mod.rs
@@ -4,10 +4,12 @@ mod b_tree;
 mod graph;
 mod heap;
 mod queue;
+mod single_linked_list;
 
 pub use self::double_linked_list::LinkedList;
 pub use self::b_tree::BTree;
 pub use self::graph::Graph;
 pub use self::heap::Heap;
 pub use self::queue::Queue;
+pub use self::single_linked_list::SingleLinkedList;
 
